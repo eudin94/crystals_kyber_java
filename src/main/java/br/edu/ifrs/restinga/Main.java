39f5bb5e75a8a99526daebc076c8e7c9cd75d736
Igor Farias <balestfarias@gmail.com>
2023-10-20 15:44:36 -0300

    encryptando mensagem
    Fazendo a encriptação de uma mensagem usando kyber.
    
    problema: não é possivel fazer a decriptação com nenhuma chave

--- src/main/java/br/edu/ifrs/restinga/Main.java
+++ src/main/java/br/edu/ifrs/restinga/Main.java
@@ -1,50 +1,58 @@
 package br.edu.ifrs.restinga;
 
+import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameters;
+import com.swiftcryptollc.crypto.interfaces.KyberPublicKey;
 import com.swiftcryptollc.crypto.provider.*;
-import com.swiftcryptollc.crypto.provider.kyber.Indcpa;
+import com.swiftcryptollc.crypto.spec.KyberGenParameterSpec;
 
-import static com.swiftcryptollc.crypto.provider.kyber.Indcpa.decrypt;
-import static com.swiftcryptollc.crypto.provider.kyber.Indcpa.encrypt;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 
 import javax.crypto.KeyAgreement;
 import java.math.BigInteger;
 import java.security.*;
-import java.util.Base64;
+import java.util.ServiceLoader;
 
+import static com.swiftcryptollc.crypto.provider.kyber.Indcpa.*;
 
 
 public class Main {
-
-    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static void main(String[] args) throws Exception {
 
         Security.setProperty("crypto.policy", "unlimited");
         Security.addProvider(new KyberJCE());
 
         String mensagem = """
-                Se tu me amas, ama-me baixinho
-                Não o grites de cima dos telhados
+                Se tu me amas, amame baixinho
+                Nao o grites de cima dos telhados
                 Deixa em paz os passarinhos
-                Deixa em paz a mim!
+                Deixa em paz a mim
                 Se me queres,
                 enfim,
                 tem de ser bem devagarinho, Amada,
-                que a vida é breve, e o amor mais breve ainda...""";
+                que a vida e breve, e o amor mais breve ainda...""";
 
-        byte[] mensagemEmBytes = mensagem.getBytes();
+        byte[] msgEmBytes = mensagem.getBytes();
 
 
         System.out.println("Alice gera um par de chaves e manda sua chave pública para Bob");
         final var aliceKeyPair = aliceGeneratesKeyPair();
 
-        System.out.println("Chave publica da alice:" +  Base64.getEncoder().encodeToString(aliceKeyPair.getPublic().getEncoded()));
-        System.out.println("Chave privada da alice:" +  Base64.getEncoder().encodeToString(aliceKeyPair.getPrivate().getEncoded()));
+        //System.out.println("Chave publica da alice:" +  Base64.getEncoder().encodeToString(aliceKeyPair.getPublic().getEncoded()));
+        //System.out.println("Chave privada da alice:" +  Base64.getEncoder().encodeToString(aliceKeyPair.getPrivate().getEncoded()));
 
         System.out.println("-------------------------------------------------");
         System.out.println("Bob gera um par de chaves");
         final var bobKeyPair = bobGeneratesKeyPair();
 
-        System.out.println("Chave publica do Bob:" +  Base64.getEncoder().encodeToString(bobKeyPair.getPublic().getEncoded()));
-        System.out.println("Chave privada do Bob:" +  Base64.getEncoder().encodeToString(bobKeyPair.getPrivate().getEncoded()));
+        //System.out.println("Chave publica do Bob:" +  Base64.getEncoder().encodeToString(bobKeyPair.getPublic().getEncoded()));
+        //System.out.println("Chave privada do Bob:" +  Base64.getEncoder().encodeToString(bobKeyPair.getPrivate().getEncoded()));
 
         System.out.println("----------------------------------------------------");
         System.out.println("Bob gera um acordo inicial de chaves a partir de sua chave privada");
@@ -54,18 +62,9 @@ public class Main {
         System.out.println("Bob gera um KyberEncrypted, carregando a chave secreta e o texto cifrado a partir da chave pública de Alice, então envia para ela o texto cifrado");
         var kyberEncrypted = bobGeneratesKyberEncrypted(bobKeyAgreement, aliceKeyPair.getPublic());
 
-        // setando a cifra no formato x.509, aceito pelo algoritmo
-        byte[] coins = {4};
-        // encripta a mensagem utilizando a chave publica do bob
-        byte[] mensagemBytesEncriptado = encrypt(mensagemEmBytes, bobKeyPair.getPublic().getEncoded(), coins, KyberKeySize.KEY_1024.getParamsK());
 
-        // transforma para a cifra do Kyber
-        KyberCipherText msgNoKyber = new KyberCipherText(mensagemBytesEncriptado, new BigInteger("2") , new BigInteger("2"));
 
-        // e adiciona a cifra do kyber no pacote de envio
-        kyberEncrypted.setCipherText(msgNoKyber);
-
-        System.out.println("Cifra Kiber: " + Base64.getEncoder().encodeToString(kyberEncrypted.getCipherText().getEncoded()));
+        //System.out.println("Cifra Kiber: " + Base64.getEncoder().encodeToString(kyberEncrypted.getCipherText().getEncoded()));
 
         System.out.println("------------------------------------------------------");
 
@@ -74,33 +73,30 @@ public class Main {
 
         System.out.println("Alice gera a mesma chave secreta a partir do texto cifrado assim gerando um KyberDecrypted");
         System.out.println("KyberDecrypted carrega a chave secreta(será a mesma que Bob gerou) e a variante");
+
         final var kyberDecrypted = aliceGeneratesKyberDecrypted(aliceKeyAgreement, kyberEncrypted.getCipherText());
 
         System.out.println("ENCRYPTED SECRET KEY = " + Base64.getEncoder().encodeToString(kyberEncrypted.getSecretKey().getEncoded()));
         System.out.println("DECRYPTED SECRET KEY = " + Base64.getEncoder().encodeToString(kyberDecrypted.getSecretKey().getEncoded()));
 
+
+        // -------------------------------------------------------
+        byte[] msgEncriptado = encrypt(msgEmBytes, bobKeyPair.getPublic().getEncoded(), msgEmBytes, KyberKeySize.KEY_1024.getParamsK());
+
+
+
+        System.out.println("Mensagem limpa: " + new String(msgEmBytes,  StandardCharsets.UTF_8));
+
+        System.out.println( "Mensagem encriptada: " + Base64.getEncoder().encodeToString(msgEncriptado));
+
+        byte[] msgDecriptado = decrypt(msgEncriptado, aliceKeyPair.getPrivate().getEncoded(), KyberKeySize.KEY_1024.getParamsK());
+
+        System.out.println("Mensagem decriptada: " + Base64.getEncoder().encodeToString(msgDecriptado));
         System.out.println("VVVVVVVVVVVVVVVVVVVVVVVVV");
 
-        // utilizando a chave criada, Alice decriptografa a mensagem recebida
-        byte[] mensagemRecebida = decrypt(kyberEncrypted.getCipherText().getC(), kyberDecrypted.getSecretKey().getEncoded(), KyberKeySize.KEY_1024.getParamsK());
-        System.out.println("Mensagem que alice recebe: " +  Base64.getEncoder().encodeToString(mensagemRecebida));
-
-        // ----------------------------
 
 
 
-        KyberPackedPKI kpPKI = new KyberPackedPKI();
-        kpPKI = Indcpa.generateKyberKeys(4);
-
-
-        System.out.println(mensagemBytesEncriptado.toString());
-
-        byte[] mensagemBytesDecriptado = decrypt(mensagemBytesEncriptado, kpPKI.getPackedPrivateKey(), KyberKeySize.KEY_1024.getParamsK());
-
-        String mensagemNova = mensagemBytesDecriptado.toString();
-        System.out.println(Base64.getEncoder().encodeToString(mensagemBytesEncriptado));
-        System.out.println();
-        System.out.println(Base64.getEncoder().encodeToString(mensagemBytesDecriptado));
 
 
 
@@ -109,11 +105,15 @@ public class Main {
     private static KeyPair aliceGeneratesKeyPair() throws NoSuchAlgorithmException {
         // "Kyber512", "Kyber768", "Kyber1024" são as opções para geração de chaves
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("Kyber1024");
+       // var kyberKeygen = new Kyber1024KeyPairGenerator();
+        //return kyberKeygen.generateKeyPair();
         return keyGen.generateKeyPair();
     }
 
     private static KeyPair bobGeneratesKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("Kyber1024");
+        //var kyberKeygen = new Kyber1024KeyPairGenerator();
+
         return keyGen.generateKeyPair();
     }
 
@@ -124,7 +124,7 @@ public class Main {
     }
 
     private static KyberEncrypted bobGeneratesKyberEncrypted(final KeyAgreement bobKeyAgreement, final PublicKey alicePublicKey) throws InvalidKeyException {
-        return (KyberEncrypted) bobKeyAgreement.doPhase(alicePublicKey, true);
+        return (KyberEncrypted) bobKeyAgreement.doPhase((KyberPublicKey) alicePublicKey, true);
     }
 
     private static KeyAgreement aliceGeneratesKeyAgreement(final PrivateKey alicePrivateKey) throws NoSuchAlgorithmException, InvalidKeyException {
@@ -134,7 +134,12 @@ public class Main {
     }
 
     private static KyberDecrypted aliceGeneratesKyberDecrypted(final KeyAgreement aliceKeyAgreement, final KyberCipherText cipherText) throws InvalidKeyException {
+
         return (KyberDecrypted) aliceKeyAgreement.doPhase(cipherText, true);
     }
+
+
+
+
 
 }
